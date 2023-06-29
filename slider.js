2cb3378ff1dbae8f244afd756717edda5adeb7ac
Henrik Andersen <han@rts.dk>
2023-06-29 12:20:16 +0200

    add navigation buttons

--- slider.js
+++ slider.js
@@ -1,6 +1,17 @@
-const sliderContainer = document.querySelector(".image-slider__images");
-const sliderButtons = document.querySelectorAll(".image-slider__radio");
+const sliderContainer = document.querySelector(".image-slider__images")
+const sliderDots = Array.from(document.querySelectorAll(".image-slider__dot"))
+const sliderButtons = document.querySelectorAll(".image-slider__button")
 
-sliderButtons.forEach((button, index) => button.addEventListener("click", () => 
+sliderDots.forEach((dot, index) => dot.addEventListener("click", () => 
     sliderContainer.scrollTo(sliderContainer.getBoundingClientRect().width * index, 0)
-));
+))
+
+sliderButtons.forEach(button => button.addEventListener("click", () => {
+    const currentIndex = sliderDots.indexOf(document.querySelector(".image-slider__dot:checked"))
+
+    if (button.classList.contains("image-slider__button--left")) {
+        sliderDots[currentIndex - 1]?.click()
+    } else {
+        sliderDots[currentIndex + 1]?.click()
+    }
+}))
